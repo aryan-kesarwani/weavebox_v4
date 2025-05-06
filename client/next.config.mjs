12e01ca8d9bd50b9b4c0727be9f21b41eb796c82
DarKFlasH07 <kesarwaniaryan76@gmail.com>
2025-05-06 21:28:30 +0530

    updated the client id path for vercel v3

--- client/next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-
-const nextConfig = {
-    env: {
-        VITE_GOOGLE_CLIENT_ID: process.env.VITE_GOOGLE_CLIENT_ID,
-    },
-};
-
-export default nextConfig;
